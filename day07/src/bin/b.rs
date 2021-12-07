7cf107492e24fa69adfc81463a593e00d6281046
R. Tony Goold <tony@goold.net>
2021-12-07 10:44:31 -0400

    Day 07

---
+++ day07/src/bin/b.rs
@@ -0,0 +1,35 @@
+use app;
+
+fn dist(a: usize, b: usize) -> usize {
+    let diff = if a >= b { a - b } else { b - a };
+    diff * (diff + 1) / 2
+}
+
+fn cost<'a>(positions: impl Iterator<Item = &'a usize>, target: usize) -> usize {
+    positions.fold(0, |acc, &x| acc + dist(x, target))
+}
+
+fn main() {
+    let line = app::read_line(&app::input_arg());
+    let positions: Vec<usize> = line
+        .split(',')
+        .map(|n| n.parse::<usize>().expect("Invalid input"))
+        .collect();
+
+    let min = *positions.iter().min().expect("Input is empty");
+    let max = *positions.iter().max().expect("Input is empty");
+
+    let mut least_cost = usize::MAX;
+    let mut least_pos = 0;
+    for i in min..=max {
+        let cur_cost = cost(positions.iter(), i);
+        if cur_cost < least_cost {
+            least_cost = cur_cost;
+            least_pos = i;
+        }
+    }
+    println!(
+        "The minimum cost is {} at position {}",
+        least_cost, least_pos
+    );
+}
