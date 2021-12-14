a88ddc519d94a7749c8f79208d7cf8baeaa0af9a
R. Tony Goold <tony@goold.net>
2021-12-14 14:14:53 -0400

    Day 14

---
+++ day14/src/bin/a.rs
@@ -0,0 +1,70 @@
+use std::collections::HashMap;
+
+use app;
+
+const NUM_STEPS: usize = 10;
+
+fn expand(s: &str, rules: &HashMap<String, String>) -> String {
+    let mut result = String::new();
+    let mut key = String::new();
+    let pairs = s.chars().zip(s.chars().skip(1));
+    for (a, b) in pairs {
+        key.clear();
+        key.push(a);
+        key.push(b);
+
+        match rules.get(&key) {
+            Some(val) => {
+                result.push(a);
+                result.push_str(val);
+            }
+            None => panic!("Failed to look up {}", &key),
+        }
+    }
+    result.push(s.chars().last().unwrap());
+    result
+}
+
+fn char_map(s: &str) -> HashMap<char, usize> {
+    let mut map = HashMap::new();
+    for c in s.chars() {
+        let entry = map.entry(c).or_default();
+        *entry += 1;
+    }
+    map
+}
+
+fn main() {
+    let mut lines = app::read_lines(&app::input_arg());
+    let mut rules: HashMap<String, String> = HashMap::new();
+    let mut chain = lines.next().expect("No input");
+    lines.next(); // Consume blank separator
+    for line in lines {
+        let mut parts = line.split(" -> ");
+        let k = parts.next().expect("Missing left hand side");
+        let v = parts.next().expect("Missing right hand side");
+        rules.insert(k.into(), v.into());
+    }
+
+    for _ in 0..NUM_STEPS {
+        chain = expand(&chain, &rules);
+    }
+
+    let map = char_map(&chain);
+    let minmax = map.iter().fold((' ', usize::MAX, ' ', usize::MIN), |acc, (k, v)| {
+        let min = if *v < acc.1 {
+            (*k, *v)
+        } else {
+            (acc.0, acc.1)
+        };
+        let max = if *v > acc.3 {
+            (*k, *v)
+        } else {
+            (acc.2, acc.3)
+        };
+        (min.0, min.1, max.0, max.1)
+    });
+    println!("The most frequent is '{}' at {}", minmax.2, minmax.3);
+    println!("The least frequent is '{}' at {}", minmax.0, minmax.1);
+    println!("The difference between most and least frequent is {}", minmax.3 - minmax.1);
+}
---
+++ day14/src/bin/b.rs
@@ -0,0 +1,110 @@
+use std::collections::HashMap;
+
+use app;
+
+const NUM_STEPS: usize = 40;
+
+#[derive(Debug, Clone)]
+enum ParseRuleError {
+    InvalidSyntax,
+    WrongSizeKey,
+    WrongSizeValue,
+}
+
+type CharCount = HashMap<char, usize>;
+
+trait Mergeable {
+    fn merge(&mut self, rhs: &Self);
+}
+
+impl Mergeable for CharCount {
+    fn merge(&mut self, rhs: &Self) {
+        for (k, v) in rhs.iter() {
+            let entry = self.entry(*k).or_default();
+            *entry += v;
+        }
+    }
+}
+
+struct Expansion {
+    memory: HashMap<(char, char, usize), CharCount>,
+    rules: HashMap<(char, char), char>,
+}
+
+impl Expansion {
+    fn new() -> Self {
+        Expansion { memory: HashMap::new(), rules: HashMap::new() }
+    }
+
+    fn add_rule(&mut self, k1: char, k2: char, v: char) {
+        self.rules.insert((k1, k2), v);
+    }
+
+    fn expand_str(&mut self, s: &str, depth: usize) -> CharCount {
+        let mut counts = CharCount::new();
+        for c in s.chars() {
+            let entry = counts.entry(c).or_default();
+            *entry += 1;
+        }
+        let pairs = s.chars().zip(s.chars().skip(1));
+        for (a, b) in pairs {
+            counts.merge(&self.expand(a, b, depth));
+        }
+        counts
+    }
+
+    fn expand(&mut self, k1: char, k2: char, depth: usize) -> CharCount {
+        if depth == 0 {
+            return CharCount::new();
+        }
+        if let Some(count) = self.memory.get(&(k1, k2, depth)) {
+            return count.clone();
+        }
+        let mut counts = CharCount::new();
+        let c = *self.rules.get(&(k1, k2)).expect("Missing rule for char pair");
+        counts.merge(&self.expand(k1, c, depth - 1));
+        counts.merge(&self.expand(c, k2, depth - 1));
+        let entry = counts.entry(c).or_default();
+        *entry += 1;
+        self.memory.insert((k1, k2, depth), counts.clone());
+        counts
+    }
+}
+
+fn parse_rule(s: &str) -> Result<(char, char, char), ParseRuleError> {
+    let mut parts = s.split(" -> ");
+    let k: Vec<char> = parts.next().map(|s| s.chars().collect()).ok_or(ParseRuleError::InvalidSyntax)?;
+    let v: Vec<char> = parts.next().map(|s| s.chars().collect()).ok_or(ParseRuleError::InvalidSyntax)?;
+    match (k.len(), v.len()) {
+        (2, 1) => Ok((k[0], k[1], v[0])),
+        (2, _) => Err(ParseRuleError::WrongSizeValue),
+        _ => Err(ParseRuleError::WrongSizeKey),
+    }
+}
+
+fn main() {
+    let mut lines = app::read_lines(&app::input_arg());
+    let input = lines.next().expect("No input");
+    lines.next(); // Consume blank separator
+
+    let mut expansion = Expansion::new();
+    lines.map(|l| parse_rule(&l).unwrap()).for_each(|(k1, k2, v)| expansion.add_rule(k1, k2, v));
+
+    let map = expansion.expand_str(&input, NUM_STEPS);
+    let minmax = map.iter().fold((' ', usize::MAX, ' ', usize::MIN), |acc, (k, v)| {
+        let min = if *v < acc.1 {
+            (*k, *v)
+        } else {
+            (acc.0, acc.1)
+        };
+        let max = if *v > acc.3 {
+            (*k, *v)
+        } else {
+            (acc.2, acc.3)
+        };
+        (min.0, min.1, max.0, max.1)
+    });
+    println!("The most frequent is '{}' at {}", minmax.2, minmax.3);
+    println!("The least frequent is '{}' at {}", minmax.0, minmax.1);
+    println!("The difference between most and least frequent is {}", minmax.3 - minmax.1);
+}
